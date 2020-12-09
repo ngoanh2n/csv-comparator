854d75b7e1919ccec6a6716322f5983965ed7498
Ngoan Ho <ngoanh2n@gmail.com>
2020-12-09 22:08:50 +0700

    [Re] Apply visitor pattern

--- src/main/java/com/github/ngoanh2n/asserts/csv/Utils.java
+++ src/main/java/com/github/ngoanh2n/asserts/csv/Utils.java
@@ -1,12 +1,31 @@
 package com.github.ngoanh2n.asserts.csv;
 
+import com.univocity.parsers.csv.CsvParser;
+import com.univocity.parsers.csv.CsvParserSettings;
+import org.mozilla.universalchardet.UniversalDetector;
+
+import javax.annotation.Nonnull;
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
- * @author ngoanh2n@gmail.com (Ho Huu Ngoan)
+ * <h3>csv-comparator<h3>
+ * <a href="https://github.com/ngoanh2n/csv-comparator">https://github.com/ngoanh2n/csv-comparator<a>
+ * <br>
+ *
+ * @author Ho Huu Ngoan (ngoanh2n@gmail.com)
+ * @since 1.0.0
  */
-
 class Utils {
 
     static File getFileFromResources(String relative) {
@@ -16,11 +35,30 @@ class Utils {
         else return new File(resource.getFile());
     }
 
-    /**
-     * Ensures that an object reference passed as a parameter to the calling method is not null
-     */
-    static <T> T checkNotNull(T reference, String format, Object... args) {
-        if (reference == null) throw new NullPointerException(String.format(format, args));
-        return reference;
+    static String timeStamp() {
+        //noinspection SpellCheckingInspection
+        return new SimpleDateFormat("yyyyMMdd.HHmmss.SSS").format(new Date());
+    }
+
+    static String charsetOf(@Nonnull File file) throws IOException {
+        return UniversalDetector.detectCharset(file);
+    }
+
+    static void createsDirectory(@Nonnull Path location) {
+        Iterator<Path> elements = location.iterator();
+        Path parentElement = Paths.get("");
+
+        while (elements.hasNext()) {
+            parentElement = parentElement.resolve(elements.next());
+            //noinspection ResultOfMethodCallIgnored
+            parentElement.toFile().mkdirs();
+        }
+    }
+
+    static List<String[]> read(@Nonnull File csv, @Nonnull Charset encoding, @Nonnull CsvParserSettings settings) {
+        checkNotNull(csv, "csv cannot be null");
+        checkNotNull(encoding, "encoding cannot be null");
+        checkNotNull(settings, "settings cannot be null");
+        return new CsvParser(settings).parseAll(csv, encoding);
     }
 }
