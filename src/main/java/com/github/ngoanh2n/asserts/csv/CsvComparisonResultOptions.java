5e0c29faa7c21013d7c18b3b7db84ada221de27d
Ngoan Ho <ngoanh2n@gmail.com>
2020-12-19 16:30:28 +0700

    Change default result location

--- src/main/java/com/github/ngoanh2n/asserts/csv/CsvComparisonResultOptions.java
+++ src/main/java/com/github/ngoanh2n/asserts/csv/CsvComparisonResultOptions.java
@@ -28,7 +28,7 @@ public interface CsvComparisonResultOptions {
 
         private Builder() {
             this.includesHeader = true;
-            this.location = Paths.get("build/blur/comparator/csv");
+            this.location = Paths.get("build/comparator/csv");
         }
 
         /**
