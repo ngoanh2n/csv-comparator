5e85bba9263a44e6923c4e0dee08bdcc06f6905a
ngoanh2n <ngoanh2n@gmail.com>
2022-12-30 15:25:59 +0700

    All modules depend on univocity-parsers

---
+++ csv-comparator/src/main/java/com/github/ngoanh2n/comparator/CsvProcessor.java
@@ -0,0 +1,79 @@
+package com.github.ngoanh2n.comparator;
+
+import com.univocity.parsers.common.ParsingContext;
+import com.univocity.parsers.common.processor.RowProcessor;
+
+import java.util.*;
+
+/**
+ * @author Ho Huu Ngoan (ngoanh2n@gmail.com)
+ * @version 1.0.0
+ * @since 2020-01-06
+ */
+class CsvRow implements RowProcessor {
+    CsvComparisonOptions options;
+    List<CsvComparisonVisitor> visitors;
+    CsvResult.Collector collector;
+    Map<String, String[]> expMap;
+    CsvSource source;
+
+    //-------------------------------------------------------------------------------//
+
+    CsvRow(CsvComparisonOptions options, List<CsvComparisonVisitor> visitors,
+           CsvResult.Collector collector, Map<String, String[]> expMap, CsvSource source) {
+        this.options = options;
+        this.visitors = visitors;
+        this.collector = collector;
+        this.expMap = expMap;
+        this.source = source;
+    }
+
+    //-------------------------------------------------------------------------------//
+
+    @Override
+    public void rowProcessed(String[] actRow, ParsingContext context) {
+        String[] expRow = expMap.get(actRow[source.getColumnId()]);
+
+        if (expRow == null) {
+            collector.rowInserted(options, source.getHeaders(), actRow);
+            visitors.forEach(v -> v.rowInserted(options, source.getHeaders(), actRow));
+        } else {
+            if (Arrays.equals(actRow, expRow)) {
+                collector.rowKept(options, source.getHeaders(), actRow);
+                visitors.forEach(v -> v.rowKept(options, source.getHeaders(), actRow));
+            } else {
+                List<HashMap<String, String>> diffs = getDiffs(source.getHeaders(), expRow, actRow);
+                collector.rowModified(options, source.getHeaders(), actRow, diffs);
+                visitors.forEach(v -> v.rowModified(options, source.getHeaders(), actRow, diffs));
+            }
+            expMap.remove(actRow[source.getColumnId()]);
+        }
+    }
+
+    @Override
+    public void processStarted(ParsingContext context) { /* No implementation necessary */ }
+
+    @Override
+    public void processEnded(ParsingContext context) { /* No implementation necessary */ }
+
+    //-------------------------------------------------------------------------------//
+
+    private List<HashMap<String, String>> getDiffs(String[] headers, String[] expRow, String[] actRow) {
+        List<HashMap<String, String>> diffs = new ArrayList<>();
+
+        for (int index = 0; index < headers.length; index++) {
+            String header = headers[index];
+            String expCell = expRow[index];
+            String actCell = actRow[index];
+
+            if (!expCell.equals(actCell)) {
+                diffs.add(new HashMap<String, String>() {{
+                    put("column", header);
+                    put("expCell", expCell);
+                    put("actCell", actCell);
+                }});
+            }
+        }
+        return diffs;
+    }
+}
