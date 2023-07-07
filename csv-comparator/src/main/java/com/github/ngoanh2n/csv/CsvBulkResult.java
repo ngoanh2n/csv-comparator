965eff8cdf9a79116948786534dad02875c44db0
ngoanh2n <ngoanh2n@gmail.com>
2023-07-07 21:07:15 +0700

    [javadoc] Add description for CsvBulkResult

--- csv-comparator/src/main/java/com/github/ngoanh2n/csv/CsvBulkResult.java
+++ csv-comparator/src/main/java/com/github/ngoanh2n/csv/CsvBulkResult.java
@@ -3,6 +3,16 @@ package com.github.ngoanh2n.csv;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * <em>Repository:</em>
+ * <ul>
+ *     <li><em>GitHub: <a href="https://github.com/ngoanh2n/csv-comparator">ngoanh2n/csv-comparator</a></em></li>
+ *     <li><em>Maven: <a href="https://mvnrepository.com/artifact/com.github.ngoanh2n/csv-comparator">com.github.ngoanh2n:csv-comparator</a></em></li>
+ * </ul>
+ *
+ * @author ngoanh2n
+ * @since 2019
+ */
 class CsvBulkResult implements CsvBulkComparisonResult {
     private final List<CsvComparisonResult> results = new ArrayList<>();
 
