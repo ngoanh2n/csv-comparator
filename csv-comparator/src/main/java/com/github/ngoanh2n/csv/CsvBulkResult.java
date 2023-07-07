7fb4d031fdc1544cc79a5f83bad9e51a8b5c479b
ngoanh2n <ngoanh2n@gmail.com>
2023-07-07 21:06:43 +0700

    [csv] Declare for CsvBulkResult

--- csv-comparator/src/main/java/com/github/ngoanh2n/csv/CsvBulkResult.java
+++ csv-comparator/src/main/java/com/github/ngoanh2n/csv/CsvBulkResult.java
@@ -1,4 +1,27 @@
 package com.github.ngoanh2n.csv;
 
-public class CsvBulkResult {
+import java.util.ArrayList;
+import java.util.List;
+
+class CsvBulkResult implements CsvBulkComparisonResult {
+    private final List<CsvComparisonResult> results = new ArrayList<>();
+
+    void put(CsvComparisonResult result) {
+        results.add(result);
+    }
+
+    @Override
+    public boolean hasDiff() {
+        return getDiffTotal() > 0;
+    }
+
+    @Override
+    public int getDiffTotal() {
+        return (int) results.stream().filter(CsvComparisonResult::hasDiff).count();
+    }
+
+    @Override
+    public List<CsvComparisonResult> getDiffResults() {
+        return results;
+    }
 }
